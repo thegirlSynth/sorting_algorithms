c4120d23785dda05c4c9edf9196c294d83b69ce2
Cynthia Uche <cynthianuchepro@gmail.com>
2022-10-08 14:47:48 -0700

    Update header file

--- sort.h
+++ sort.h
@@ -25,6 +25,8 @@ typedef struct listint_s
 } listint_t;
 
 /* Prototypes */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
