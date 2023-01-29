0c37e19b7ab14a9528bd618a58d4b719b1a20b5d
Cynthia Uche <cynthianuchepro@gmail.com>
2023-01-28 18:38:03 -0800

    Update header file

--- sort.h
+++ sort.h
@@ -4,11 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Prototypes */
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
 
-/* Data Structure */
+/* Data Structures */
+
+/* Structure for doubly linked list */
 
 /**
  * struct listint_s - Doubly linked list node
@@ -17,9 +16,21 @@ void print_list(const listint_t *list);
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+/* Prototypes */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_node(listint_t *swap, listint_t *node);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
